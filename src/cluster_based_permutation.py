65e85669ea0bd46e9c92bd3ecb66a5696091cfb6
ClaraKuper <clara-kuper@web.de>
2023-09-27 11:41:11 +0200

    integrate 3-dimensional support

--- src/cluster_based_permutation.py
+++ src/cluster_based_permutation.py
@@ -1,5 +1,4 @@
 import warnings
-
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -20,14 +19,13 @@ def cluster_based_permutation_test(condition_a, condition_b, critical_t, n_reps,
     # clusters in our data
     condition_difference = condition_a - condition_b
     if condition_difference.shape[0]>condition_difference.shape[1]:
-        warnings.WarningMessage(f"The data frame seems to have more rows (repetitions of the measurement) - "
+        print(f"The data frame seems to have more rows (repetitions of the measurement) - "
                                 f"{condition_difference.shape[0]} rows - "
                                 f"than columns (time points) - {condition_difference.shape[1]} columns - in the "
                                 f"measurement. \nPlease make sure that this is correct.")
     t_values = t_stats(condition_difference)
     clusters = find_clusters(t_values, critical_t)
     cluster_df = pd.DataFrame.from_dict(clusters).T
-
     permutated_clusters, cutoff_value = random_permutation(condition_difference, critical_t, n_reps, percentile, random_seed)
     cluster_over_thresh = cluster_df[cluster_df['cluster_weight'] > cutoff_value]
 
@@ -47,7 +45,13 @@ def random_permutation(data, critical_t, n_reps, percentile, random_seed):
     permutated_cluster_df = pd.DataFrame(columns = ['clusterID', 'nRep', 'value'])
 
     for rep in range(n_reps):
-        permutated_data = data.mul(random_permutation_matrix[rep], axis = 0)
+        if len(data.shape) == 2:
+            permutated_data = data.mul(random_permutation_matrix[rep], axis = 0)
+        if len(data.shape) == 3:
+            permutated_data = []
+            for idx, part in enumerate(data):
+                permutated_data.append(part*random_permutation_matrix[rep][idx])
+            permutated_data = np.array(permutated_data)
         t_values = t_stats(permutated_data)
         clusters = find_clusters(t_values, critical_t)
         if len(clusters)>0:
@@ -76,7 +80,7 @@ def get_random_permutation_matrix(nrow, ncol, random_seed):
     random_permutation_matrix = np.sign(random_permutation_matrix)
     return random_permutation_matrix
 
-def find_clusters(values_to_compare, critical_value):
+def find_clusters(values_to_compare, critical_value, ignore_inf = True):
     """
     find a cluster of values above the critial threshold
     """
@@ -87,11 +91,17 @@ def find_clusters(values_to_compare, critical_value):
     for cluster in range(n_clusters):
         cluster_id = cluster+1
         cluster_location = np.where(over_critical_positions == cluster_id)
-        current_cluster = values_to_compare.values[cluster_location]
+        current_cluster = values_to_compare[cluster_location]
+        if ignore_inf:
+            current_cluster = current_cluster[abs(current_cluster)!=np.inf]
         all_clusters[cluster] = {}
         all_clusters[cluster]['cluster_id'] = cluster_id
         all_clusters[cluster]['cluster_size'] = len(current_cluster)
-        all_clusters[cluster]['cluster_weight'] = abs(sum(current_cluster))
+        all_clusters[cluster]['cluster_weight'] = abs(np.nansum(current_cluster))
+        if all_clusters[cluster]['cluster_weight'] == np.inf or all_clusters[cluster]['cluster_weight'] == np.nan:
+            raise ValueError(f"The cluster weight could not be computed. "
+                             f"Cluster weight is {all_clusters[cluster]['cluster_weight']}")
+
         all_clusters[cluster]['cluster_location'] = cluster_location
     return all_clusters
 
@@ -100,4 +110,4 @@ def t_stats(values):
     """
     takes a data frame (values) and returns the t-statistic across rows
     """
-    return np.mean(values, axis = 0)/(np.std(values, axis = 0)/values.shape[0])
+    return np.nanmean(values, axis = 0)/(np.nanstd(values, axis = 0)/values.shape[0])
